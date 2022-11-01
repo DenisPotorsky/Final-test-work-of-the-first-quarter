cbdf00971e6a6df618465ff4231dbee1535ffda9
Den <denis-s2@yandex.ru>
2022-11-01 12:50:29 +0500

    Начинаем решение задачи

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+WriteLine("Заполните массив через пробел");
+string[] array = ReadLine()!.Split(' ', StringSplitOptions.RemoveEmptyEntries);
