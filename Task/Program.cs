5bbb2bec48140585bfa1a340b0cfb4e91994bbb3
Den <denis-s2@yandex.ru>
2022-11-01 12:52:38 +0500

    написал метод FillingNewArray

--- Task/Program.cs
+++ Task/Program.cs
@@ -15,3 +15,20 @@ int NewArrayLength(string[] arr)
     }
     return count;
 }
+
+string[] FillingNewArray(string[] array, int len)
+{
+    int index = 0;
+    string[] newArr = new string[len];
+    for (int i = 0; i < array.Length; i++)
+    {
+        string str = array[i];
+        int length = str.Length;
+        if (length <= 3)
+        {
+            newArr[index] = array[i];
+            index++;
+        }
+    }
+    return newArr;
+}
