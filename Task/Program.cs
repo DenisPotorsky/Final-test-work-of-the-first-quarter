7832a64edfbd4bf9f98529141e108ff4ad9d88b5
Den <denis-s2@yandex.ru>
2022-11-01 12:51:38 +0500

    написал метод NewArrayLength

--- Task/Program.cs
+++ Task/Program.cs
@@ -3,3 +3,15 @@ Clear();
 
 WriteLine("Заполните массив через пробел");
 string[] array = ReadLine()!.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+int NewArrayLength(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        string str = arr[i];
+        int length = str.Length;
+        if (length <= 3) count++;
+    }
+    return count;
+}
